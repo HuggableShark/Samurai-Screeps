589c7020d6ecc0659702fc60128e25bb147836b7
HuggableShark <sruiter333@gmail.com>
2019-03-27 23:27:24 -0400

    - Improved tower script: Better logic and has alarm system
    - Testing new storage withdraw logic for worker creeps.

--- role.waller.js
+++ role.waller.js
@@ -20,6 +20,7 @@ module.exports = {
             var walls = creep.room.find(FIND_STRUCTURES, {
                 filter: (s) => s.structureType == STRUCTURE_WALL
             });
+            // find all ramparts in the room
             var ramparts = creep.room.find(FIND_STRUCTURES, {
                 filter: (s) => s.structureType == STRUCTURE_RAMPART
             });
@@ -28,9 +29,15 @@ module.exports = {
 
             // loop with increasing percentages
             for (let percentage = 0.00001; percentage <= 1; percentage = percentage + 0.00001){
-                for (let wall of walls ) {
+                for (let wall of walls) {
                     if (wall.hits / wall.hitsMax < percentage) {
                         target = wall;
+                        break
+                    }
+                }
+                for (let rampart of ramparts) {
+                    if (rampart.hits / rampart.hitsMax < percentage) {
+                        target = rampart;
                         break
                     }
                 }
@@ -58,14 +65,40 @@ module.exports = {
         // if creep is supposed to harvest energy from source
         else {
             // find closest source
-            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE, {
+            var sourcesInRoom = creep.room.find(FIND_SOURCES);
+            var closestSource = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE, {
                 filter: (s) => s.energy > 0
             });
+
+// #############################################################################
+            // Testing out code to have creep look at storage if source in room
+            // has less than 25% energy left
+// #############################################################################
+
+            var weakSource = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE, {
+              filter: (s) => s.energy < s.energyCapacity * 0.25
+            });
+            var warehouse = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+              filter: (s) => (s.structureType == STRUCTURE_CONTAINER
+                          || s.structureType == STRUCTURE_STORAGE)
+                          && s.store.energy > 0
+            });
+
+            // If the closest source has < 25% energy & there is stored energy
+            if (weakSource != undefined && warehouse != undefined) {
+              if (creep.withdraw(warehouse, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(warehouse);
+              }
+            }
+
+
+
+
             // try to harvest energy, if the source is not in range
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                // move towards the source
-                creep.moveTo(source);
+            if (creep.harvest(closestSource) == ERR_NOT_IN_RANGE) {
+              // move towards the source
+              creep.moveTo(closestSource);
             }
         }
     }
-};
+}
